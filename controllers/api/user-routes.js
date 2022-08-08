5eaf9306134f609e0c2e5d6b640629ca005d6f17
marlowe <marlowepdx@gmail.com>
2022-08-07 21:32:47 -0700

    comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {username: 'Username', email: 'example@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
