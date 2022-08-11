85aba2112fadaf7b28abbfc48aedb765e8f1f006
Jonsey <mwjones2186@gmail.com>
2022-08-10 20:00:05 -0700

    updated routes and website is rendering a page.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
-const hobbyRoutes = require('./hobby-routes');
 const tagRoutes = require('./tag-routes');
 
 router.use('/categories', categoryRoutes);
-router.use('/hobbies', hobbyRoutes);
 router.use('/tags', tagRoutes);
 
 module.exports = router;
