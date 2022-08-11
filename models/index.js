dea8be9809c9b59e489e984065d7c5ad1fa5226c
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-08-10 20:32:52 -0700

    added in comment, Comment, comment-seeds, and comment-routes; also index.js for models

---
+++ models/index.js
@@ -0,0 +1,43 @@
+const Post = require("./Post");
+const User = require("./User");
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
