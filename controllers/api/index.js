fd77c59562c065d47daf17bca44a516745601134
lorreina <slorreina369@gmail.com>
2022-08-13 11:59:52 -0700

    comment route connected to api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const categoryRoutes = require('./category-routes');
 const tagRoutes = require('./tag-routes');
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/category', categoryRoutes);
 router.use('/tags', tagRoutes);
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
