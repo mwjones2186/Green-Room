6831450dceb2103399a5d503f1c595c3820da4be
marlowe <marlowepdx@gmail.com>
2022-08-13 17:02:03 -0700

    bootstrap changes, replaced document query classes with ids

--- public/js/login.js
+++ public/js/login.js
@@ -45,9 +45,9 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
+  .querySelector('#signup-form')
   .addEventListener('submit', signupFormHandler);
