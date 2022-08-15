1976d30b938f891a12f6db14a8eb4e40d730a08d
lorreina <slorreina369@gmail.com>
2022-08-15 16:27:11 -0700

    implement functional messageboard

--- controllers/category-routes.js
+++ controllers/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Category} = require('../models');
+const {Category, Post, User} = require('../models');
 
 router.get('/', (req,res) =>{
     Category.findAll({raw: true})
@@ -21,10 +21,37 @@ router.get('/', (req,res) =>{
 });
 
 router.get('/:id', async (req, res) => {
-    res.render('category/posts', {
-        loggedIn: req.session.loggedIn,
-        user_id: req.session.user_id,
-    });
+    Category.findOne({
+        where:{
+            id:req.params.id
+        },
+        attributes:['category_name'],
+        include:{
+            model:Post,
+            attributes:['id','title', 'body'],
+            include:{
+                model:User,
+                attributes:['username']
+            }
+        }
+    })
+    .then(dbCategoryData => {
+        if(!dbCategoryData){
+            res.status(400).json({message:'Category not found!'});
+            return
+        }
+        const category = dbCategoryData.get({plain: true});
+        console.log(category.posts)
+        res.render('category/posts', {
+            category,
+            loggedIn: req.session.loggedIn,
+            user_id: req.session.user_id,
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 
