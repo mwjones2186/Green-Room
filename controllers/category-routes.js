31983c703019f26cd46cc37821d22c19efdb4c88
lorreina <slorreina369@gmail.com>
2022-08-15 22:45:02 -0700

    Add Post button created for each category

--- controllers/category-routes.js
+++ controllers/category-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
         where:{
             id:req.params.id
         },
-        attributes:['category_name'],
+        attributes:['id','category_name'],
         include:{
             model:Post,
             attributes:['id','title', 'body'],
