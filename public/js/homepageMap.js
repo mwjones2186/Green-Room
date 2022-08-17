4d0bcc2b70b5a86c6af44b49cf4d58ae91026575
Jonsey <mwjones2186@gmail.com>
2022-08-16 21:22:52 -0700

    integrated map working.

--- public/js/homepageMap.js
+++ public/js/homepageMap.js
@@ -1,27 +1,37 @@
 
-const googleMapEl = document.getElementById("googleMap")
+const googleMapEl = document.getElementById("map")
 const zipcode=document.getElementById("zipcode").value
 
 // `https://maps.googleapis.com/maps/api/geocode/json?address=${req.session.zipcode}&key=${process.env.gMapsAPI}`
 
 
 function init(){
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=AIzaSyDiGnQ61aAehxeFqd9DfJ6JZ8OOBVuruQU`).then((data)=>{
-      return data.json()
-    }).then((res)=>{
-        console.log(res)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=AIzaSyDiGnQ61aAehxeFqd9DfJ6JZ8OOBVuruQU`).then(function (results) {
+        return results.json()
+    }).then(function (data) {
+        
+        let lat = data.results[0].geometry.location.lat;
+        let lng = data.results[0].geometry.location.lng;
+        initMap(lat, lng)
     })
+
 }
+
 init()
 
-function myMap(lat, lon) {
-var mapProp= {
-  center: new google.maps.LatLng$(lat,-lon),
-  zoom:12,
-};
-var map = new google.maps.Map(googleMapEl,mapProp);
-}
-
-
-
+function initMap(lat, lng) {
+    // The location of Uluru
+    const usermap = {lat, lng};
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: usermap,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: usermap,
+      map: map,
+    });
+  }
+  
 
